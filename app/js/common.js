af02278fff6b9431c34fdd33febd76efb97a5205
viktoriazaharova <zaharova181114@gmail.com>
2020-07-20 19:47:21 +0300

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -16,6 +16,64 @@ $('.tooltip-wrapper').hover(function () {
    $(this).find('.tooltip-hidden').fadeToggle();
 });
 
+
+$('.works-slider').slick({
+    slidesToShow: 1,
+    dots: true,
+    appendArrows: '.works-slider__nav',
+    appendDots: '.works-slider__nav',
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>'
+});
+
+
+$('.playpause').click(function () {
+    $('.video-wrapper').toggleClass('video-click');
+
+    if($(".fullscreen-video").get(0).paused){
+        $(".fullscreen-video").get(0).play();
+        $(this).fadeOut();
+
+    }else{
+        $(".fullscreen-video").get(0).pause();
+
+        $(this).fadeIn();
+    }
+
+});
+
+var target = $('.benefits__inner');
+var targetPos = target.offset().top;
+var winHeight = $(window).height();
+var scrollToElem = targetPos - winHeight;
+$(window).scroll(function(){
+    var winScrollTop = $(this).scrollTop();
+    if(winScrollTop > scrollToElem){
+        $({ blurRadius: 5 }).animate(
+            { blurRadius: 0 },
+            {
+                duration: 3500,
+                easing: "swing",
+            }
+        );
+        var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(
+            " "
+        );
+        $(".benefits__number").each(function () {
+            var tcount = $(this).data("count");
+            $(this).animateNumber(
+                {
+                    number: tcount,
+                    easing: "easeInQuad",
+                    numberStep: comma_separator_number_step
+                },
+                1000
+            );
+        });
+    }
+});
+
+
 // timer
 function getTimeRemaining(endtime) {
     var t = Date.parse(endtime) - Date.parse(new Date());
@@ -58,3 +116,4 @@ function initializeClock(id, endtime) {
 
 var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
 initializeClock('countdown', deadline);
+
